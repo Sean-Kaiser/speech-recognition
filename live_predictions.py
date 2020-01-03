02fe9f658c0946102986d7b3a3daba11b6ad03e8
Sean-Kaiser <thekaiserbun@gmail.com>
2020-01-02 17:46:32 -0800

    Add new digit classifier using mfcc from librosa as feature selector - 95%+ cv accuracy. Add VAD as check before classifying live

---
+++ live_predictions.py
@@ -0,0 +1,30 @@
+import numpy as np
+from keras.models import model_from_json
+from librosa.feature import mfcc
+
+
+# load json and create model
+json_file = open('digit_classifier.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+classifier = model_from_json(loaded_model_json)
+# load weights into new model
+classifier.load_weights("digit_classifier.h5")
+print("Loaded model from disk")
+
+
+def predict_1d(data, fs):
+    
+    X = np.zeros((1,len(data),1))
+    X[0,:,0] = data
+    
+    return classifier.predict(X).argmax(1)
+    #return random.choice([0,1,2,3,4,5,6,7,8,9])
+
+def predict_2d(data, fs):
+    
+    X = np.zeros((1,20,32,1)) #Length of mfcc from training
+    
+    X[0,:,:,0] = mfcc(data, fs)
+    
+    return classifier.predict(X).argmax(1)
--- main.py
+++ main.py
@@ -1,35 +1,68 @@
 import numpy as np
 import pyaudio
 import matplotlib.pyplot as plt
-  
+import live_predictions as lp
+import webrtcvad as vd
+
+
 
 #Initialize PyAudio              
 p = pyaudio.PyAudio()
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 8000 #Time resolution of recording device
-CHUNK =16000 #Datapoints to read at a time
+RATE = 8000 #Sampling rate
+CHUNK = 1000 #Datapoints to read at a time
+pred_interval = 2 #Seconds to predict on
+disp_interval = 10
+frame_duration = 30 #ms to be used for VAD
+samp_width = 2 #Used for VAD
 
 
 #Create open stream
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK)
+audio_data = np.zeros((int(RATE*pred_interval)))
+predictions = np.zeros(10)
+vad = vd.Vad(1) #Voice activity detector with aggressiveness set
 
 
 #Create Figure
+t = np.linspace(0,9,10)
 fig, ax = plt.subplots(figsize=(14,6))
-x = np.arange(0, 2 * CHUNK, 2)
-ax.set_ylim(-200, 200)
-ax.set_xlim(0, CHUNK) #make sure our x axis matched our chunk size
-line, = ax.plot(x, np.random.rand(CHUNK))
+x = np.linspace(0,9,10)
+ax.set_ylim(-5,10)
+line, = ax.step(x, np.zeros((len(predictions))))
 
-#Automatically update the Figure
+
+#While continuously collecting data
 while True:
+    
+    #Get audio data
     data = stream.read(CHUNK)
-    data = np.frombuffer(data, np.int16)
-    line.set_ydata(data)
+    current_data = np.frombuffer(data, np.int16)
+        
+    #Rotat data and fill
+    audio_data = np.roll(audio_data, -CHUNK) #negative to roll opposite direction
+    audio_data[-CHUNK:] = current_data
+    
+    #If voice activity then predict
+    if vad.is_speech(data[-int(frame_duration/1000*RATE*samp_width):], RATE):
+        
+        #Predict on data
+        prediction = lp.predict_2d(audio_data, RATE)
+    
+    else:       
+        prediction = -1
+        
+    #Update Predictions
+    predictions = np.roll(predictions, -1)
+    predictions[-1] = prediction
+    
+    #Plot Prediction Outputs    
+    line.set_ydata(predictions)
     fig.canvas.draw()
     fig.canvas.flush_events()
-    plt.pause(0.01)
+    plt.pause(0.01)    
+    
 
 
 
